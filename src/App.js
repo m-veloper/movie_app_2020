708f1116dd42f2bc7570813c2d64fe16eae36946
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-06-23 01:13:43 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Potato from "./Potato";
+import { Button } from '@material-ui/core';
+
 
 
 function App() {
   return (
     <div className="App">
+      Hello
+      <Potato />
+      <Button color="primary">버튼</Button>
     </div>
   );
 }
