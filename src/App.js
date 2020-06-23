c80f76d0ccbf194660cda7f2867a7fd718b1b63b
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-06-24 01:03:09 +0900

    ic

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Potato from "./Potato";
 import { Button } from '@material-ui/core';
 
-
+function Food({fav}) {
+    return <h1>포테이토 좋아 {fav}</h1>
+}
 
 function App() {
   return (
     <div className="App">
       Hello
-      <Potato />
-      <Button color="primary">버튼</Button>
+      <Food fav="kimchi"/>
+      <Food fav="1"/>
+      <Food fav="2"/>
+      <Food fav="3"/>
+      <Food fav="4"/>
     </div>
   );
 }
--- src/Potato.js
+++ src/Potato.js
@@ -1,9 +1,9 @@
-import  React from "react";
-
-function Potato() {
-    return(
-        <h3> I Love potato</h3>
-    )
-}
-
-export  default Potato;
+// import  React from "react";
+//
+// function Potato() {
+//     return(
+//         <h3> I Love potato</h3>
+//     )
+// }
+//
+// export  default Potato;
