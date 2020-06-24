ee39962ca87e0392c5ba74c2acbc674d5e5c2f8c
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-06-25 01:15:14 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -2,19 +2,50 @@ import React from 'react';
 import Potato from "./Potato";
 import { Button } from '@material-ui/core';
 
-function Food({fav}) {
-    return <h1>포테이토 좋아 {fav}</h1>
+function Food({ name, picture }) {
+    return (
+        <div>
+            <h2>I like {name}</h2>
+            <img src={picture} />
+        </div>
+    );
 }
+
+const foodILike = [
+    {
+        name: "Kimchi",
+        image:
+            "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+    },
+    {
+        name: "Samgyeopsal",
+        image:
+            "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+    },
+    {
+        name: "Bibimbap",
+        image:
+            "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+    },
+    {
+        name: "Doncasu",
+        image:
+            "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+    },
+    {
+        name: "Kimbap",
+        image:
+            "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+    }
+];
 
 function App() {
   return (
     <div className="App">
       Hello
-      <Food fav="kimchi"/>
-      <Food fav="1"/>
-      <Food fav="2"/>
-      <Food fav="3"/>
-      <Food fav="4"/>
+        {foodILike.map(dish => (
+            <Food name={dish.name} picture={dish.image} />
+        ))}
     </div>
   );
 }
