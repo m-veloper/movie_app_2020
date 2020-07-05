b5c2c5d8c3d683a0996e369e10c3e11b60a8ded2
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-07-06 00:33:00 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
-class  App extends React.Component{
+class App extends React.Component {
     state = {
-        count: 0
+        isLoading: true
+    };
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({isLoading: false})
+        }, 6000);
     }
-    add = () => {
-        this.setState(current => ({ count: current.count + 1 }));
-    }
-    minus = () => {
-        this.setState(current => ({ count: current.count - 1 }));
-    }
-    render(){
-        return (
-            <div>
-                <h1>The number is {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
-            </div>
-        )
+
+    render() {
+        const { isLoading } = this.state;
+        return <div>{this.state.isLoading ? "Loading" : "We are ready"}</div>
     }
 }
-
-
 
 export default App;
