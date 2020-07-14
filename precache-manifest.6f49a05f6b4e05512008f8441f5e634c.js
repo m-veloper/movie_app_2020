2ba7a46a31c6d1bfa4e508dc08ad06a354faae08
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-07-15 00:54:19 +0900

    Updates

---
+++ precache-manifest.6f49a05f6b4e05512008f8441f5e634c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244b1b42df29e37d789f62b2cd9334e1",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "75e314c4f639e341171c",
+    "url": "/movie_app_2020/static/css/main.87491607.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75e314c4f639e341171c",
+    "url": "/movie_app_2020/static/js/main.d3b68c35.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
